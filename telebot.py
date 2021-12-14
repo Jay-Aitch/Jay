f992ec407c696de1bfb637581d6e651f1990847e
Jay-Aitch <95948146+Jay-Aitch@users.noreply.github.com>
2021-12-14 23:06:29 +0900

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -1,6 +1,6 @@
 import telepot # telepot 모듈 import 
-token = "5069562183:AAHVCjQit7rWLHii2wUjOu3q_9T1MbuwQzE" # 봇 API 입력 
-# mc = "454439308" # 텔레그램 숫자 ID 입력 
-mc = "-771094944"
+token = "" # 봇 API 입력 
+# mc = "" # 텔레그램 숫자 ID 입력 
+mc = ""
 bot = telepot.Bot(token) 
 bot.sendMessage(mc, "test") # 할말 적어서 메시지 보내기 
