9b68f96312e1446a527d6b9c1c1d5f5b719b1898
Jay-Aitch <95948146+Jay-Aitch@users.noreply.github.com>
2022-01-28 23:02:28 +0900

    Add files via upload

--- b_at22.py
+++ b_at22.py
@@ -172,7 +172,7 @@ while True:
             
             if slow_k_30m[-1] <= 90 and slow_k_30m[-1] > slow_d_30m[-1] :
                 print(now,"long")
-                if slow_k_5m[-1] <= 80 and slow_k_5m[-1] > slow_d_5m[-1]  :
+                if slow_k_5m[-1] <= 80 and slow_k_5m[-2] > slow_d_5m[-2] and slow_k_5m[-1] > slow_d_5m[-1] :
                     #  slow_k_1m[-2] <= 70 slow_k_1m[-2] <= slow_d_1m[-2]
                     if macd_5m[-2] <= macd_signal_5m[-2] and macd_5m[-1] > macd_signal_5m[-1]  :
                         position['type'] = 'long'
@@ -220,7 +220,7 @@ while True:
             #short조건1                    slow_k_30m[-2] > slow_k_30m[-1] and macd_osc_30m[-2] > macd_osc_30m[-1]and macd_5m[-2] > macd_5m[-1] and macd_osc_5m[-2] > macd_osc_5m[-1]and slow_k_30m[-2] > slow_d_30m[-2]
             if slow_k_30m[-1] >= 10 and slow_k_30m[-1] < slow_d_30m[-1]  :
                 print(now,"short")
-                if slow_k_5m[-1] >= 20 and slow_k_5m[-1] < slow_d_5m[-1]  :
+                if slow_k_5m[-1] >= 20 and slow_k_5m[-2] < slow_d_5m[-2] and slow_k_5m[-1] < slow_d_5m[-1]  :
                     # 
                     if macd_5m[-2] >= macd_signal_5m[-2] and macd_5m[-1] < macd_signal_5m[-1]  :
                         position['type'] = 'short'
